f562c3f10ae26e6594f40af9acc4745b3493472f
Vladyslav1992 <vladpleten@gmail.com>
2018-07-09 11:19:22 +0300

    add Helpers

---
+++ addressbook-web-tests/GroupHelper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace WebAdressbookTests
+{
+    public class GroupHelper
+    {
+        private IWebDriver driver;
+
+        public GroupHelper(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void InitNewGroupCreation()
+        {
+            driver.FindElement(By.Name("new")).Click();
+        }
+
+        public void FillGroupForm(GroupData group)
+        {
+            driver.FindElement(By.Name("group_name")).Click();
+            driver.FindElement(By.Name("group_name")).Clear();
+            driver.FindElement(By.Name("group_name")).SendKeys(group.Name);
+            driver.FindElement(By.Name("group_header")).Click();
+            driver.FindElement(By.Name("group_header")).Clear();
+            driver.FindElement(By.Name("group_header")).SendKeys(group.Header);
+            driver.FindElement(By.Name("group_header")).Click();
+            driver.FindElement(By.Name("group_footer")).Clear();
+            driver.FindElement(By.Name("group_header")).SendKeys(group.Footer);
+        }
+
+        public void SubmitGroupCretion()
+        {
+            driver.FindElement(By.Name("group_footer")).SendKeys("TestGroup");
+            driver.FindElement(By.Name("submit")).Click();
+        }
+
+        public void ReturnToGroupsPage()
+        {
+            driver.FindElement(By.LinkText("group page")).Click();
+        }
+
+        public void SelectGroup(int index)
+        {
+            driver.FindElement(By.XPath("(//input[@name='selected[]'])[" + index + "]")).Click();
+        }
+
+        public void RemoveGroup()
+        {
+            driver.FindElement(By.Name("delete")).Click();
+        }
+    }
+}
