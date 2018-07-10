d76ffa5266a88f5d8662685d6a54a2a16a6509ba
Vladyslav1992 <vladpleten@gmail.com>
2018-07-10 19:06:47 +0300

    Change void type on GroupHelper in GroupHelper class

--- addressbook-web-tests/appmanager/GroupHelper.cs
+++ addressbook-web-tests/appmanager/GroupHelper.cs
@@ -18,12 +18,13 @@ namespace WebAdressbookTests
             this.driver = driver;
         }
 
-        public void InitNewGroupCreation()
+        public GroupHelper InitNewGroupCreation()
         {
             driver.FindElement(By.Name("new")).Click();
+            return this;
         }
 
-        public void FillGroupForm(GroupData group)
+        public GroupHelper FillGroupForm(GroupData group)
         {
             driver.FindElement(By.Name("group_name")).Click();
             driver.FindElement(By.Name("group_name")).Clear();
@@ -34,27 +35,32 @@ namespace WebAdressbookTests
             driver.FindElement(By.Name("group_header")).Click();
             driver.FindElement(By.Name("group_footer")).Clear();
             driver.FindElement(By.Name("group_header")).SendKeys(group.Footer);
+            return this;
         }
 
-        public void SubmitGroupCretion()
+        public GroupHelper SubmitGroupCretion()
         {
             driver.FindElement(By.Name("group_footer")).SendKeys("TestGroup");
             driver.FindElement(By.Name("submit")).Click();
+            return this;
         }
 
-        public void ReturnToGroupsPage()
+        public GroupHelper ReturnToGroupsPage()
         {
             driver.FindElement(By.LinkText("group page")).Click();
+            return this;
         }
 
-        public void SelectGroup(int index)
+        public GroupHelper SelectGroup(int index)
         {
             driver.FindElement(By.XPath("(//input[@name='selected[]'])[" + index + "]")).Click();
+            return this;
         }
 
-        public void RemoveGroup()
+        public GroupHelper RemoveGroup()
         {
             driver.FindElement(By.Name("delete")).Click();
+            return this;
         }
     }
 }
